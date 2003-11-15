8f9c7e1277ceb7dfae236c9971591bcb4a5e2e14
sparkyx <dany118@gmail.com>
2003-11-15 10:39:47 +0000

    Adaptation for user profile New feature : create database modele

--- html/admin_repo.php
+++ html/admin_repo.php
@@ -24,7 +24,6 @@ include_once("postgres.php");
 include_once("debug.php");
 html_page_start($g_UserProperty['use_theme']);
 echo_debug("entering admin_repo");
-//echo_debug("user $g_user");
 
 CheckUser();
 if ($g_UserProperty['use_admin'] != 1) {
@@ -33,7 +32,7 @@ if ($g_UserProperty['use_admin'] != 1) {
 }
 include ("top_menu_compta.php");
 
-echo '<H2 style="color:blue;text-align:center"> Administration Globale</H2>';
+echo '<H2 class="info"> Administration Globale</H2>';
 if ( ! isset ($g_dossier) ) $g_dossier=0;
 ShowMenuAdminGlobalRight($g_dossier);
 ShowMenuAdminGlobal();
@@ -109,7 +108,7 @@ if ( isset ($_GET["action"]) ) {
       // Database created
       $l_id=GetDbId($dos);
       if ( $l_id != 0) {
-	$Sql=sprintf("CREATE DATABASE DOSSIER%d encoding='ISO8859-1' TEMPLATE templ_account",$l_id);
+	$Sql=sprintf("CREATE DATABASE DOSSIER%d encoding='ISO8859-1' TEMPLATE MOD%d",$l_id,$_POST["FMOD_ID"]);
 	echo_debug($Sql);
 	ExecSql($cn,$Sql);
       } // if $l_id != 0
@@ -133,23 +132,131 @@ if ( $Res != null ) {
   }
 
   echo "</TR>";
+  // Load the available Templates
+
+  $Res=ExecSql($cn,"select mod_id,mod_name,mod_desc from 
+                      modeledef order by mod_name");
+  $count=pg_NumRows($Res);
+  $template="";
+  if ( $count == 0 ) {
+    echo "No template available";
+  } else {
+    $template.='<SELECT NAME=FMOD_ID>';
+      for ($i=0;$i<$count;$i++) {
+	$mod=pg_fetch_array($Res,0);
+	$template.='<OPTION VALUE="'.$mod['mod_id'].'"> '.$mod['mod_name']." - ".$mod['mod_desc'];
+	printf("<span> %s  %s </span>",
+	       $mod['mod_name'],$mod['mod_desc']);
+
+      }// for
+      $template.="</SELECT>";
+    }// if count = 0
+
 }
 // Add a new folder
 ?>
 </TABLE>
-<TABLE>
  <FORM ACTION="admin_repo.php?action=dossier_mgt" METHOD="POST">
- <TD> Name  <INPUT TYPE="TEXT" NAME="DATABASE"> </TD>
- <TD> Description <INPUT TYPE="TEXT" NAME="DESCRIPTION" SIZE="30"> </TD>
- <TD> <INPUT TYPE=SUBMIT VALUE="Create Folder"></TD>
+    <TABLE>
+    <TR>
+    <TD> Name  <INPUT TYPE="TEXT" NAME="DATABASE"> </TD>
+    <TD> Description <INPUT TYPE="TEXT" NAME="DESCRIPTION" SIZE="30"> </TD>
+    <TD> <? echo $template; ?> </TD>
+    </TR>
+    <TR>
+    <TD> <INPUT TYPE=SUBMIT VALUE="Create Folder"></TD>
+    </TR>
+    </TABLE>
  </FORM>
- </TR>
-</TABLE>
-
 
 <?
 
   } // action = dossier_mgt
+  if ( $_GET["action"] == "modele_mgt" ) {
+    $cn=DbConnect();
+
+    // IF FMOD_NAME is posted then must add a template
+    if ( isset ($_POST["FMOD_NAME"]) ) {
+      $mod_name=FormatString($_POST["FMOD_NAME"]);
+      $mod_desc=FormatString($_POST["FMOD_DESC"]);
+      if ( $mod_name != null) {
+	$Res=ExecSql($cn,"insert into modeledef(mod_name,mod_desc)
+                        values ('".$mod_name."',".
+		     "'".$mod_desc."')");
+	
+	// get the mod_id
+	$l_id=GetModeleId($cn,$mod_name);
+	if ( $l_id != 0 ) {
+	  $Sql=sprintf("CREATE DATABASE MOD%d encoding='ISO8859-1' TEMPLATE DOSSIER%s",$l_id,$_POST["FMOD_DBID"]);
+	  ExecSql($cn,$Sql);
+	}
+      }// if $mod_name != null
+      // Clean some tables 
+      $Res=ExecSql($cn,"truncate table jrn");
+      $Res=ExecSql($cn,"truncate table jrnx");
+      $Res=ExecSql($cn,"truncate table centralized");
+      // Reset Sequence
+      $Res=ExecSql($cn,"drop sequence s_jrn");
+      $Res=ExecSql($cn,"drop sequence s_jrn_op");
+      $Res=ExecSql($cn,"drop sequence s_centralized");
+
+      $Res=ExecSql($cn,"create sequence s_jrn");
+      $Res=ExecSql($cn,"create sequence s_jrn_op");
+      $Res=ExecSql($cn,"create sequence s_centralized");
+
+    }
+    // Show all available templates
+
+    $Res=ExecSql($cn,"select mod_id,mod_name,mod_desc from 
+                      modeledef order by mod_name");
+    $count=pg_NumRows($Res);
+    if ( $count == 0 ) {
+      echo "No template available";
+    } else {
+     
+      for ($i=0;$i<$count;$i++) {
+	$mod=pg_fetch_array($Res,0);
+	printf("<span> %s  %s </span>",
+	       $mod['mod_name'],$mod['mod_desc']);
+
+      }// for
+    }// if count = 0
+    // Show All available folder
+    $Res=ExecSql($cn,"select dos_id, dos_name,dos_description from ac_dossier
+                      order by dos_name");
+    $count=pg_NumRows($Res);
+    $available="";
+    if ( $count != 0 ) {
+      $available='<SELECT NAME="FMOD_DBID">';
+      for ($i=0;$i<$count;$i++) {
+	$db=pg_fetch_array($Res,$i);
+	$available.='<OPTION VALUE="'.$db['dos_id'].'">'.$db['dos_name'].':'.$db['dos_description'];
+      }//for i
+      $available.='</SELECT>';
+    }//if count !=0
+?>
+<form action="admin_repo.php?action=modele_mgt" METHOD="post">
+<TABLE>
+<tr>
+    <td>Nom </TD>
+    <TD><INPUT TYPE="TEXT" VALUE="" NAME="FMOD_NAME"></TD>
+</TR>
+<TR>
+    <TD>Description</TD>
+    <TD><INPUT TYPE="TEXTAREA" ROWS="4" COLS="30" NAME="FMOD_DESC"></TD>
+</TR>
+<TR>
+    <TD> Basé sur </TD>
+    <TD> <? echo $available ?></TD>
+</TR>
+<TR>
+    <td colspan="2"> <INPUT TYPE="SUBMIT" VALUE="Add a template"></TD>
+</TR>
+</TABLE>
+</form>
+<?
+
+  }// action = modele_mgt
 } // action is set
 ?>
 </DIV>
