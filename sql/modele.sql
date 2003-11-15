1cc94d7344c073a55824acf5dfd036dd9208c630
sparkyx <dany118@gmail.com>
2003-11-15 10:20:22 +0000

    New Feature add database modele

---
+++ sql/modele.sql
@@ -0,0 +1,25 @@
+/* $Revision$ */
+/*
+ *   This file is part of WCOMPTA.
+ *
+ *   WCOMPTA is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   WCOMPTA is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with WCOMPTA; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+create sequence s_modid;
+
+create table modeledef (
+       mod_id integer not null default nextval('s_modid') primary key,
+       mod_name text not null,
+       mod_desc text
+);
