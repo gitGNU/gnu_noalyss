b92c637d0d6544440180ab3c8215a5cc38aaeccc
sparkyx <dany118@gmail.com>
2003-11-30 13:18:31 +0000

    Database change

--- sql/version.sql
+++ sql/version.sql
@@ -17,4 +17,4 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 create table version ( val integer);
-insert into version values (1);
+insert into version values (3);
