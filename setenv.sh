6176a18a33f35497760fb8df7a1c6bbc49bba1eb
sparkyx <dany118@gmail.com>
2004-06-06 10:19:01 +0000

    remove personal info

--- setenv.sh
+++ setenv.sh
@@ -2,7 +2,6 @@ export COMPTA_HOME=/home/httpd/compta
 export PGDATA=/opt/database
 export PATH=/opt/psql732/bin:$PATH
 export MANPATH=/opt/psql732/man
-export PATH=$PATH:/home/danpo/doxygen/bin
 #
 # Owner of the database
 #
