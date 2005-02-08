8419edba4dd9e3ac32406057b37e673fcfa0d3c8
sparkyx <dany118@gmail.com>
2005-02-08 10:58:19 +0000

    IMPROVE setup html based, ability to patch

--- html/admin/setup.php
+++ html/admin/setup.php
@@ -41,7 +41,47 @@ include_once('postgres.php');
 include_once('debug.php');
 include_once('ac_common.php');
 include_once('variable.php');
+/* function ExecuteScript
+ **************************************************
+ * Purpose : Execute a sql script
+ *        
+ * parm : 
+ *	- $p_cn database connection
+ *      - $script script name
+ * gen :
+ *	- none
+ * return:
+ *        none
+ */
+function ExecuteScript($p_cn,$script) {
+  $hf=fopen($script,'r');
+  $sql="";
+  while (!feof($hf)) {
+    $buffer=fgets($hf);
+    // comment are not execute
+    if ( strrpos($buffer,"--") == true) {
+      continue;
+    }
+    // Blank Lines Are Skipped
+    If ( Strlen($buffer)==0) {
+      Echo "Blank";
+      Continue;
+    }
 
+    // No semi colon -> multiline command
+    if ( strpos($buffer,';') == false ) {
+      $sql.=$buffer;
+      continue;
+    } 
+    // cut the semi colon
+    $buffer=str_replace (';','',$buffer);
+    $sql.=$buffer;
+    print "Execute $sql <hr>";
+    ExecSql($p_cn,$sql);
+    $sql="";
+  } // while (feof)
+  fclose($hf);
+}
 
 // Verify some PHP parameters
 // magic_quotes_gpc = Off
@@ -67,7 +107,7 @@ if ( ereg("\.\.\/include",ini_get('include_path')) == false )
 $cn=DbConnect(-2,'phpcompta');
 
 if ($cn == false ) {
-  print "<p> Vous devez absolument taper dans une console la commande 'createuser -d phpcompta'
+  print "<p> Vous devez absolument taper dans une console la commande 'createuser -d phpcompta -P dany'
   puis  la commande 'createdb -O phpcompta phpcompta'. </p>
 <p>Ces commandes créeront l'utilisateur phpcompta
 puis la base de données par défaut de phpcompta.</p>";
@@ -87,12 +127,20 @@ if ($account == 0 ) {
   $r=system("$psql -U phpcompta account_repository -f sql/account_repository/data.sql",$r);
   echo "Creation of Démo";
   ExecSql($cn,"create database dossier1 encoding='latin1'");
-  $r=system("$psql -U phpcompta dossier1 -f sql/dossier1/schema.sql",$r);
-  $r=system("$psql -U phpcompta dossier1 -f sql/dossier1/data.sql",$r);
+//   $r=system("$psql -U phpcompta dossier1 -f sql/dossier1/schema.sql",$r);
+//   $r=system("$psql -U phpcompta dossier1 -f sql/dossier1/data.sql",$r);
+  $cn=DbConnect(1,'dossier');
+  ExecuteScript($cn,'sql/dossier1/schema.sql');
+  ExecuteScript($cn,'sql/dossier1/data.sql');
+
   echo "Creation of Modele1";
   ExecSql($cn,"create database mod1 encoding='latin1'");
-  $r=system("$psql -U phpcompta mod1 -f sql/mod1/schema.sql",$r);
-  $r=system("$psql -U phpcompta mod1 -f sql/mod1/data.sql",$r);
+  $cn=DbConnect(1,'mod');
+  ExecuteScript($cn,'sql/mod1/schema.sql');
+  ExecuteScript($cn,'sql/mod1/data.sql');
+
+//   $r=system("$psql -U phpcompta mod1 -f sql/mod1/schema.sql",$r);
+//   $r=system("$psql -U phpcompta mod1 -f sql/mod1/data.sql",$r);
   ob_end_clean();
 
 }
@@ -108,3 +156,27 @@ if ( ($Res=ExecSql($a,"select  * from ac_users") ) == false ) {
 } else 
 	print "Connect to database success <br>";
 echo "<h2 class=\"info\"> Congratulation : Test successfull</h2>";
+
+echo "<h2 class=\"info\"> Patching databases</h2>";
+
+$cn=DbConnect();
+$Res=ExecSql($cn,"select dos_id, dos_name from ac_dossier");
+$MaxDossier=pg_NumRows($Res);
+
+for ($e=0;$e < $MaxDossier;$e++) {
+  $db_row=pg_fetch_array($Res,$e);
+  echo "Patching ".$db_row['dos_name'];
+  if ( GetVersion($db_row['dos_id']) == 3 ) { 
+    $db=DbConnect($db_row['dos_id'],'dossier');
+    ExecuteScript($db,'sql/patch/upgrade4.sql');
+      
+    $sql="select jrn_def_id from jrn_def ";
+    $Res=ExecSql($db,$sql);
+    $Max=pg_NumRows($Res);
+    for ($seq=0;$seq<$Max;$seq++) {
+	    $row=pg_fetch_array($Res,$seq);
+	    $sql=sprintf ("create sequence s_jrn_%d",$row['jrn_def_id']);
+	    ExecSql($db,$sql);
+    }
+ } // version == 3
+ }
