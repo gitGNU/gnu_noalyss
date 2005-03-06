9b4e864b058b175b6c84abf78bbfbcea966ff9d1
Stan Pinte <stanpinte@yahoo.fr>
2005-03-06 16:47:34 +0000

    made script useful for other dbs, and file ids

--- scripts/add-db.sh
+++ scripts/add-db.sh
@@ -1,4 +1,6 @@
-createdb -h localhost -U phpcompta dossier3
-psql -h localhost -U phpcompta dossier3 -f phpcompta_11022005.sql
+NR_DOSSIER=$1
+BACKUP_FILE=$2
+createdb -h localhost -U phpcompta dossier$NR_DOSSIER
+psql -h localhost -U phpcompta dossier$NR_DOSSIER -f $BACKUP_FILE
 psql -h localhost -U phpcompta --command "INSERT INTO ac_users (use_login, use_active, use_pass, use_admin, use_usertype) VALUES ('stan', 1, md5('plogastel'), 1, 'user')" account_repository
-psql -h localhost -U phpcompta --command "INSERT INTO ac_dossier (dos_id, dos_name, dos_description) VALUES (3, 'Sauvages', 'Comptabilité Sauvages SPRL')" account_repository
+psql -h localhost -U phpcompta --command "INSERT INTO ac_dossier (dos_id, dos_name, dos_description) VALUES ($NR_DOSSIER, 'Sauvages', 'Comptabilité Sauvages SPRL')" account_repository
