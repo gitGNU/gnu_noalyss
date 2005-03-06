92cfa223cb60e9dcf0550381f22dca5977f53f90
Stan Pinte <stanpinte@yahoo.fr>
2005-03-06 14:50:30 +0000

    Set focus directly on user field

--- html/index.php
+++ html/index.php
@@ -12,22 +12,22 @@ BODY {
 }
 </style> 
 </head>
-<BODY>
+<BODY onLoad="document.loginform.p_user.focus()">
 <BR><BR><BR><BR><BR>
 <center>
 	<IMG SRC="image/logo7.jpg" alt="Logo">
 <BR>
 
-<form action="login.php" method="post">
+<form action="login.php" method="post" name="loginform">
 <TABLE><TR><TD>
 <TABLE  BORDER=0 CELLSPACING=0>
 <TR>
 <TD class="cell">Login</TD>
-<TD><input type="text" name="p_user"></TD>
+<TD><input type="text" name="p_user" tabindex="1"></TD>
 </TR>
 <TR>
 <TD> Password</TD>
-<TD><INPUT TYPE="PASSWORD" NAME="p_pass"></TD>
+<TD><INPUT TYPE="PASSWORD" NAME="p_pass" tabindex="2"></TD>
 </TR>
 <TR>
 <TD COLSPAN=2 ALIGN=CENTER><INPUT TYPE=SUBMIT value="Log in"></TD>
