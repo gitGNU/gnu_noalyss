d84fc602bcb170221c388277af17d5b638b6af01
sparkyx <dany118@gmail.com>
2005-03-31 20:40:33 +0000

    Task 3953 Missing Menus

--- html/line_update.php
+++ html/line_update.php
@@ -41,12 +41,49 @@ if ( $User->CheckAction(DbConnect($_SESSION['g_dossier']),MPCMN) == 0 ) {
  }
 
 include ("user_menu.php");
-ShowMenuComptaRight($_SESSION['g_dossier'],$User->admin);
-$l_Db=sprintf("dossier%d",$_SESSION['g_dossier']);
-echo '<DIV CLASS="ccontent">';
+
+/* Modif d'une ligne */
+if ( isset ($_POST["update"] ) ) {
+  foreach ($HTTP_POST_VARS as $name => $element) {
+    echo_debug(__FILE__,__LINE__,"name $name $element");
+  }
+  echo JS_UPDATE_PCMN;
+  $cn=DbConnect($_SESSION['g_dossier']);
+    $p_val=trim($_POST["p_val"]);
+    $p_lib=FormatString($_POST["p_name"]);
+    $p_parent=trim($_POST["p_val_parent"]);
+    $old_line=trim($_POST["p_old"]);
+    echo_debug(__FILE__,__LINE__,"Update old : $old_line News = $p_val $p_lib");
+    if ( strlen ($p_val) != 0 && strlen ($p_lib) != 0 && strlen($old_line)!=0 ) {
+      if (strlen ($p_val) == 1 ) {
+	$p_parent=0;
+      } else {
+	if ( strlen($p_parent)==0 ) {
+	  $p_parent=substr($p_val,0,strlen($p_val)-1);
+	  echo_debug(__FILE__,__LINE__,"Modif valeur = $p_val parent = $p_parent");
+	}
+      }
+      /* Parent existe */
+      $Ret=ExecSql($cn,"select pcm_val from tmp_pcmn where pcm_val=$p_parent");
+      if ( pg_NumRows($Ret) == 0 || $p_parent==$old_line ) {
+	echo '<SCRIPT> alert(" Ne peut pas modifier; aucune poste parent"); </SCRIPT>';
+      } else {
+	$Ret=ExecSql($cn,"update tmp_pcmn set pcm_val=$p_val, pcm_lib='$p_lib',pcm_val_parent=$p_parent where pcm_val=$old_line");
+      }
+    } else {
+      echo '<script> alert(\'Update Valeurs invalides\'); </script>';
+    }
+
+  echo "<script> 
+        window.close();
+         self.opener.RefreshMe();
+
+        </script>";
+
+}
 
 ?>
-<FORM ACTION="pcmn_update.php" METHOD="POST">
+<FORM ACTION="line_update.php" METHOD="POST">
 <TABLE>
 <TR>
 <?
@@ -61,15 +98,16 @@ echo "</TR><TR>";
 printf ('<TD>Classe Parent</TD><TD><INPUT TYPE="TEXT" name="p_val_parent" value="%s"></TD>',$p);
 ?>
 </TR>
+</TABLE>
+<TABLE>
 <TR>
-<TD><INPUT TYPE="Submit" VALUE="Save">
+<TD><INPUT TYPE="Submit" VALUE="Sauve">
 <INPUT TYPE="HIDDEN" name="update">
 <? printf ('<INPUT TYPE="HIDDEN" name="p_old" value="%s">',$l); ?>
-</TD></TR>
-<TR> <TD class="mtitle"><A CLASS="mtitle" HREF="pcmn_update.php">Retour sans sauver</A></TD></TR>
+</TD><TD><input type="button"  Value="Retour sans sauver" onClick='window.close();'></TD></TR>
 </TABLE>
 </FORM>
-</DIV>
+
 
 <?
 html_page_stop();
--- html/pcmn_update.php
+++ html/pcmn_update.php
@@ -20,6 +20,8 @@
 /* $Revision$ */
 include_once ("ac_common.php");
 html_page_start($_SESSION['use_theme']);
+require_once("constant.php");
+
 if ( ! isset ( $_SESSION['g_dossier'] ) ) {
   echo "You must choose a Dossier ";
   phpinfo();
@@ -44,7 +46,7 @@ if ( $User->CheckAction($cn,MPCMN) == 0 ) {
   NoAccess();
   exit -1;			
  }
-
+echo JS_UPDATE_PCMN;
 /* Store the p_start parameter */
 if ( ! isset ( $_SESSION['g_start']) ) {
   $_SESSION['g_start']="";
@@ -111,38 +113,6 @@ if ( isset ( $_POST["Add"] ) ) {
     }
   }
 }
-/* Modif d'une ligne */
-if ( isset ($_POST["update"] ) ) {
-  foreach ($HTTP_POST_VARS as $name => $element) {
-    echo_debug(__FILE__,__LINE__,"name $name $element");
-  }
-    $p_val=trim($_POST["p_val"]);
-    $p_lib=FormatString($_POST["p_name"]);
-    $p_parent=trim($_POST["p_val_parent"]);
-    $old_line=trim($_POST["p_old"]);
-    echo_debug(__FILE__,__LINE__,"Update old : $old_line News = $p_val $p_lib");
-    if ( strlen ($p_val) != 0 && strlen ($p_lib) != 0 && strlen($old_line)!=0 ) {
-      if (strlen ($p_val) == 1 ) {
-	$p_parent=0;
-      } else {
-	if ( strlen($p_parent)==0 ) {
-	  $p_parent=substr($p_val,0,strlen($p_val)-1);
-	  echo_debug(__FILE__,__LINE__,"Modif valeur = $p_val parent = $p_parent");
-	}
-      }
-      /* Parent existe */
-      $Ret=ExecSql($cn,"select pcm_val from tmp_pcmn where pcm_val=$p_parent");
-      if ( pg_NumRows($Ret) == 0 || $p_parent==$old_line ) {
-	echo '<SCRIPT> alert(" Ne peut pas modifier; aucune poste parent"); </SCRIPT>';
-      } else {
-	$Ret=ExecSql($cn,"update tmp_pcmn set pcm_val=$p_val, pcm_lib='$p_lib',pcm_val_parent=$p_parent where pcm_val=$old_line");
-      }
-    } else {
-      echo '<script> alert(\'Update Valeurs invalides\'); </script>';
-    }
-  
-
-}
 
 $Ret=ExecSql($cn,"select pcm_val,pcm_lib,pcm_val_parent from tmp_pcmn where substr(pcm_val::text,1,1)='".$_SESSION['g_start']."' order by pcm_val::text");
 $MaxRow=pg_NumRows($Ret);
@@ -183,7 +153,12 @@ for ($i=0; $i <$MaxRow; $i++) {
   echo $A['pcm_val'];
 
   echo "</td> $td";
-  printf ("<A HREF=line_update.php?l=%d&n=%s&p=%s>",$A['pcm_val'],urlencode($A['pcm_lib']),$A['pcm_val_parent']);
+  //  printf ("<A HREF=line_update.php?l=%d&n=%s&p=%s>",$A['pcm_val'],urlencode($A['pcm_lib']),$A['pcm_val_parent']);
+  printf ("<A HREF=\"javascript:PcmnUpdate(%d,'%s','%s','%s')\">",
+	  $A['pcm_val'],
+	  FormatString($A['pcm_lib']),
+	  $A['pcm_val_parent'],
+	  $_REQUEST['PHPSESSID']);
   echo $A['pcm_lib'];
   echo '</A>';
   echo "</TD>";
