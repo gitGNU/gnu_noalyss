0cfc8c15ac475972ba610807b544af6f7dc2784e
Stan Pinte <stanpinte@yahoo.fr>
2005-04-21 17:01:53 +0000

    Made sure upgrade4.sql is processed on new file

--- scripts/add-db.sh
+++ scripts/add-db.sh
@@ -8,4 +8,4 @@ psql -h localhost -U phpcompta -f html/admin/sql/patch/upgrade4.sql dossier$NR_D
 psql -h localhost -U phpcompta --command "INSERT INTO ac_users (use_login, use_active, use_pass, use_admin, use_usertype) VALUES ('$USER_NAME', 1, md5('plogastel'), 1, 'user')" account_repository
 psql -h localhost -U phpcompta --command "INSERT INTO ac_dossier (dos_id, dos_name, dos_description) VALUES ($NR_DOSSIER, 'Sauvages', 'Comptabilité Sauvages SPRL')" account_repository
 psql -h localhost -U phpcompta --command "INSERT INTO jnt_use_dos (use_id, dos_id) SELECT use_id,$NR_DOSSIER from ac_users WHERE use_login='$USER_NAME'" account_repository
-
+psql -h localhost -U phpcompta -f html/admin/sql/patch/upgrade4.sql dossier$NR_DOSSIER
