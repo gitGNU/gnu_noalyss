0ebfa5b0c6d994ee4ae6feaa5655f7de4f128975
sparkyx <dany118@gmail.com>
2005-09-03 07:19:03 +0000

    Add a operation without  tva

--- html/admin/sql/patch/upgrade6.sql
+++ html/admin/sql/patch/upgrade6.sql
@@ -1,6 +1,7 @@
 
 --make sure that p_start < p_end 
 ALTER TABLE parm_periode ADD CHECK (p_end >= p_start);
+insert into tva_rate values (5,'0%',0, 'Pas soumis à la TVA',null);
 
 
 update  version set val=7;
