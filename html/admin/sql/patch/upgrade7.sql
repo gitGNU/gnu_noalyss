b9676fd2a615410e9491cffa8208c09f51300b0b
sparkyx <dany118@gmail.com>
2005-09-25 16:20:53 +0000

    Patch 7 progress

--- html/admin/sql/patch/upgrade7.sql
+++ html/admin/sql/patch/upgrade7.sql
@@ -1,4 +1,10 @@
 begin;
+
+insert into tva_rate values (5,'0%',0, 'Pas soumis à la TVA',null);
+
+update fiche_def_ref set frd_class_base=2400 where frd_id=7;
+
+update  version set val=8;
 -- banque n'a pas de gestion stock
 delete from jnt_fic_attr where fd_id=1 and ad_id=19;
 -- client n'a pas de gestion stock
