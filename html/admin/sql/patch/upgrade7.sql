52a8f041608f9d2bf4569bd7bafdce0f3772c312
sparkyx <dany118@gmail.com>
2005-10-18 12:43:42 +0000

    add a needed check on the col j_tech_per and jr_tech_per

--- html/admin/sql/patch/upgrade7.sql
+++ html/admin/sql/patch/upgrade7.sql
@@ -13,6 +13,10 @@ delete from jnt_fic_attr where fd_id=2 and ad_id=19;
  update user_pref set pref_periode=40 where pref_user='phpcompta';
 -- create index ix_j_grp on jrnx(j_grpt);
 -- create index ix_jr_grp on jrn(jr_grpt_id);
+update jrnx set j_tech_per = jr_tech_per  from jrn  where j_grpt=jr_grpt_id and j_tech_per is null;
+alter table jrnx alter j_tech_per set not null;
+alter table jrn alter jr_tech_per set not null;
 -- version 8
+
 update version set val=8;
 commit;
