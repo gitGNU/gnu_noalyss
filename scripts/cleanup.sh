f827dc7b45ed47622bc79ddf2f26e51175925e34
sparkyx <dany118@gmail.com>
2005-10-30 21:15:42 +0000

    adapt script to domaine

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # clean all phpcompta related DB.
 
-dropdb -U phpcompta -h localhost account_repository
-dropdb -U phpcompta -h localhost dossier1
-dropdb -U phpcompta -h localhost mod1
+dropdb -U phpcompta -h localhost dev_account_repository
+dropdb -U phpcompta -h localhost dev_dossier1
+dropdb -U phpcompta -h localhost dev_mod1
+dropdb -U phpcompta -h localhost dev_mod2
+
