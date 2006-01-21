e91269af989f83f107464329c10f1f837c8cec0e
Stan Pinte <stanpinte@yahoo.fr>
2006-01-21 22:37:16 +0000

    Added Ctrl-Enter scripts. To bind, just set ID='SubmitButton' on an input button

--- html/scripts.js
+++ html/scripts.js
@@ -17,6 +17,15 @@ function GetFormField(Field) {
   return elem.elements ? elem.elements[Field] : false;
 }
 
+function AttachEvent(Obj, EventType, Function, Capture) {
+  Capture = Capture || false;
+  if (Obj == document && !document.all) { Obj = window; }
+  if (Obj.addEventListener) {
+    Obj.addEventListener(EventType, Function, Capture);
+  } else if (Obj.attachEvent) {
+    Obj.attachEvent("on" + EventType, Function);
+  }
+}
 
 function SetFocus(Field,SelectIt) {
   var elem = GetFormField(Field);
@@ -46,3 +55,12 @@ function SetFocus(Field,SelectIt) {
   return true;
 }
 
+function HandleSubmit(e) {
+  SubmitButton = GetID('SubmitButton');
+  var code = e.charCode || e.keyCode;
+  if ( (code == 13) && e.ctrlKey ) 
+  {     
+    SubmitButton.click();    
+    return true;
+  }
+}
