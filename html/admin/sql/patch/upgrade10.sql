cd1f6468132236b04a527038eca2f732a36a7d0f
sparkyx <dany118@gmail.com>
2006-04-28 22:19:17 +0000

    fix a problem in the function dump

--- html/admin/sql/patch/upgrade10.sql
+++ html/admin/sql/patch/upgrade10.sql
@@ -14,7 +14,7 @@ begin
 	tt=NEW.jr_grpt_id;
 	diff:=check_balance(tt);
 	if diff != 0 then
-		raise exception 'Rounded error %',diff ;
+		raise exception 'balance error %',diff ;
 	end if;
 	return NEW;
 	end if;
@@ -65,6 +65,76 @@ end;
 $BODY$
   LANGUAGE plpgsql VOLATILE;
 
+-- add quick_code to the vw_client view
+drop view vw_client;
+
+
+create view vw_client as 
+ SELECT a.f_id, a.av_text AS name, a1.av_text as quick_code,b.av_text AS tva_num, c.av_text AS poste_comptable, d.av_text AS rue, e.av_text AS code_postal, f.av_text AS pays, g.av_text AS telephone, h.av_text AS email
+   FROM ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+           FROM fiche
+      JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 1) a
+   inner JOIN ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+           FROM fiche
+      JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 13) b USING (f_id)
+   inner JOIN ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+   FROM fiche
+   JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 23) a1 USING (f_id)
+   inner JOIN ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+      FROM fiche
+   JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 5) c USING (f_id)
+   inner JOIN ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+   FROM fiche
+   JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 14) d USING (f_id)
+   inner JOIN ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+   FROM fiche
+   JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 15) e USING (f_id)
+   inner JOIN ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+   FROM fiche
+   JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 16) f USING (f_id)
+   inner JOIN ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+   FROM fiche
+   JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 17) g USING (f_id)
+   LEFT JOIN ( SELECT jnt_fic_att_value.jft_id, fiche.f_id, fiche_def.frd_id, fiche.fd_id, fiche_def.fd_class_base, fiche_def.fd_label, fiche_def.fd_create_account, fiche_def_ref.frd_text, fiche_def_ref.frd_class_base, jnt_fic_att_value.ad_id, attr_value.av_text
+   FROM fiche
+   JOIN fiche_def USING (fd_id)
+   JOIN fiche_def_ref USING (frd_id)
+   JOIN jnt_fic_att_value USING (f_id)
+   JOIN attr_value USING (jft_id)
+  WHERE jnt_fic_att_value.ad_id = 18) h USING (f_id)
+  WHERE a.frd_id = 9;
 
 
 
