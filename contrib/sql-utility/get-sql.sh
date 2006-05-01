9644afd840cca92a1dc63274774b6d098a6d7b83
sparkyx <dany118@gmail.com>
2006-05-01 18:11:55 +0000

    *** empty log message ***

--- contrib/sql-utility/get-sql.sh
+++ contrib/sql-utility/get-sql.sh
@@ -8,6 +8,7 @@ Help () {
 	cat <<_eof
 $0 [option] database
 	-f function only
+	-t table only
 _eof
 }
 
@@ -19,4 +20,9 @@ fi
 case "$1" in 
 	-f)
 		pg_dump -s "$2"|awk '/CREATE FUNCTION/,/LANGUAGE/ { print $0;}' 
+		;;
+	-t)
+		pg_dump -s "$2"| awk '/CREATE TABLE/,/;/ { print $0;}' 
+		pg_dump -s "$2"| awk '/ALTER TABLE/,/;/ { print $0;}' 
+		;;
 esac
