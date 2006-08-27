fb3fb31a7d9aa09b842d5c208fdf67ad00a0b862
sparkyx <dany118@gmail.com>
2006-08-27 16:00:31 +0000

    Bug 17544 double attribute

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,4 +1,5 @@
 begin;
+
 create or replace function correct_sequence ( p_sequence text,p_col text, p_table text )
 returns integer
 as
@@ -45,6 +46,19 @@ comment on function correct_sequence (text,text,text) is ' Often the primary key
  the value of the sequence is not synchronized with the primary key ( p_sequence : sequence name, p_col : col of the pk,p_table : concerned table';
 
 select correct_sequence('s_jnt_fic_att_value','jft_id','jnt_fic_att_value');
+
+-- add a pk to the table jnt_fic_attr
+alter table jnt_fic_attr add jnt_id int8;
+create sequence s_jnt_id;
+alter table jnt_fic_attr alter jnt_id set default nextval('s_jnt_id');
+update jnt_fic_attr set jnt_id=nextval('s_jnt_id');
+alter table jnt_fic_attr add constraint pk_jnt_fic_attr primary key (jnt_id);
+
+-- remove duplicate attr
+delete  from jnt_fic_attr where jnt_id in ( select a.jnt_id from jnt_fic_attr a join jnt_fic_attr b on (a.fd_id=b.fd_id and a.ad_id=b.ad_id) where a.jnt_id > b.jnt_id); 
+
+
+
 
 
 
