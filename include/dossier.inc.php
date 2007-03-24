95998f06c4d8e18a6080391965b98fe1ee0bc151
sparkyx <dany118@gmail.com>
2007-03-24 19:47:06 +0000

    Move and Add warning

---
+++ include/dossier.inc.php
@@ -0,0 +1,203 @@
+<?
+/*
+ *   This file is part of PhpCompta.
+ *
+ *   PhpCompta is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   PhpCompta is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with PhpCompta; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/* $Revision$ */
+
+// Copyright Author Dany De Bontridder ddebontridder@yahoo.fr
+
+/* !\file 
+ */
+
+/* \brief Management of the folder
+ *
+ */
+
+echo '<div class="u_redcontent">';
+// check and add an new folder
+if ( isset ($_POST["DATABASE"]) ) {
+  $cn=DbConnect();
+  $dos=trim($_POST["DATABASE"]);
+      if (strlen($dos)==0) {
+	echo ("Dataname name is empty");
+	exit -1;
+      }
+      $desc=FormatString($_POST["DESCRIPTION"]);
+      $Res=ExecSql($cn,"insert into ac_dossier(dos_name,dos_description)
+                    values ('".$dos."','$desc')");
+      // If the id is not null, name successfully inserted
+      // Database created
+      $l_id=GetDbId($dos);
+      if ( $l_id != 0) {
+	//--
+	// setting the year
+	//--
+	$year=FormatString($_POST['YEAR']);
+	if ( strlen($year) != 4 || isNumber($year) == 0 || $year > 2100 || $year < 2000 || $year != round($year,0))
+	  {
+	    echo "$year est une année invalide";
+	    $Res=ExecSql($cn,"delete from ac_dossier where dos_id=$l_id");
+	  }
+	else 
+	  {
+	    $Sql=sprintf("CREATE DATABASE %sDOSSIER%d encoding='ISO8859-1' TEMPLATE %sMOD%d",
+			 domaine,
+			 $l_id,
+			 domaine,
+			 $_POST["FMOD_ID"]);
+	    echo_debug($Sql);
+	    ob_start();
+	    if ( pg_query($cn,$Sql)==false) {
+		ob_clean();
+	    	ExecSql($cn,"delete from ac_dossier where dos_id=$l_id");
+		echo "<h2 class=\"error\"> Base de donnée ".domaine."mod".$_POST['FMOD_ID']."  est accèdée, déconnectez-vous d'abord</h2>";
+		exit;
+		}
+	     ob_flush();
+	    $Res=ExecSql($cn,"insert into jnt_use_dos (use_id,dos_id) values (1,$l_id)");
+	    // Connect to the new database
+	    $cn=DbConnect($l_id);
+	    //---
+	    // Cleaning Action
+	    //-- 
+	    if ( isset($_POST['DOC'] ))
+	      {
+		$Res=ExecSql($cn,"delete from action_gestion");
+		$Res=ExecSql($cn,"delete from document");
+	      }
+	    if ( isset($_POST['CARD'])) 
+	      {
+		$Res=ExecSql($cn,"delete from  attr_value");
+		$Res=ExecSql($cn,"delete from  jnt_fic_att_value");
+		$Res=ExecSql($cn,"delete from   fiche");
+		$Res=ExecSql($cn,"delete from action_gestion");
+		$Res=ExecSql($cn,"delete from document");
+
+
+	      }
+	    //--year --
+	    $Res=ExecSql($cn,"delete from parm_periode");
+	    if ( ($year % 4 == 0 && $year % 100 != 0) || $year % 400 == 0 )  
+	      $fev=29;
+	    else
+	      $fev=28;
+
+	    $Res=ExecSql($cn,"delete from user_local_pref where parameter_type='PERIODE'");
+	    $nb_day=array(31,$fev,31,30,31,30,31,31,30,31,30,30);
+	    $m=1;
+	    foreach ($nb_day as $day) 
+	      {
+		$p_start=sprintf("01-%d-%s",$m,$year);
+		$p_end=sprintf("%d-%d-%s",$day,$m,$year);
+		$sql=sprintf("insert into parm_periode (p_start,p_end,p_exercice)
+                              values (to_date('%s','DD-MM-YYYY'),to_date('%s','DD-MM-YYYY'),'%s')",
+			     $p_start,$p_end,$year);
+		$Res=ExecSql($cn,$sql);
+		$m++;
+	      }
+	    $sql=sprintf("insert into parm_periode (p_start,p_end,p_exercice)
+                              values (to_date('31-12-%s','DD-MM-YYYY'),to_date('31-12-%s','DD-MM-YYYY'),'%s')",
+			     $year,$year,$year);
+	    $Res=ExecSql($cn,$sql);
+
+
+	  }
+      } // if $l_id != 0
+    } // $_POST[DATABASE]
+?>
+   <h2> Dossier Management</h2>
+
+<?
+      
+      $cn=DbConnect();
+    $offset=(isset($_REQUEST['offset']))?$_REQUEST['offset']:0;
+    $page=(isset($_REQUEST['page']))?$_REQUEST['page']:1;
+    $count=getDbValue($cn,"select count(*) from ac_dossier");
+    $size=10;
+
+    echo jrn_navigation_bar($offset,$count,$size,$page); 
+    $Res=ShowDossier('all',$offset,$size);
+    $compteur=1;
+    $template="";
+    echo JS_CONFIRM;
+    echo '<TABLE BORDER=1>';
+
+    // show all dossiers
+    if ( $Res != null ) {
+      foreach ( $Res as $Dossier) {
+	
+	if ( $compteur%2 == 0 ) 
+	  $cl='class="odd"';
+	else
+	  $cl='class="even"';
+
+	echo "<TR $cl><TD VALIGN=\"TOP\"> 
+<B>$Dossier[dos_name]</B> </TD>
+<TD><I>  ".$Dossier['dos_description']."</I>
+</TD>
+<TD>
+<input type=\"button\" name=\"Effacer\"".
+'Value="Effacer" onClick="confirm_remove(\''.$_REQUEST['PHPSESSID'].'\',\''.$Dossier['dos_id'].'\',\'db\');" \>'.
+	       '</td>'.
+"</TD></TR>";
+
+	$compteur++; 
+	
+      }
+
+      echo "</TR>";
+      
+    }
+    
+    // Load the available Templates
+    $Res=ExecSql($cn,"select mod_id,mod_name,mod_desc from 
+                      modeledef order by mod_name");
+    $count=pg_NumRows($Res);
+    
+if ( $count == 0 ) {
+  echo "No template available";
+} else {
+  $template='<SELECT NAME=FMOD_ID>';
+  for ($i=0;$i<$count;$i++) {
+    $mod=pg_fetch_array($Res,$i);
+    $template.='<OPTION VALUE="'.$mod['mod_id'].'"> '.$mod['mod_name']." - ".substr($mod['mod_desc'],0,30);
+  }// for
+      $template.="</SELECT>";
+}// if count = 0
+ $m_date=date('Y');
+// Add a new folder
+?>
+</TABLE>
+    <? echo jrn_navigation_bar($offset,$count,$size,$page); ?>
+ <FORM ACTION="admin_repo.php?action=dossier_mgt" METHOD="POST">
+    <TABLE>
+    <TR>
+    <TD> Nom du dossier</td><td>  <INPUT TYPE="TEXT" NAME="DATABASE"> </TD>
+    </TR><TR>
+    <TD> Description</td><td>  <TEXTAREA COLS="60" ROWS="2" NAME="DESCRIPTION" ></TEXTAREA> </TD>
+    </TR>
+    <TR> <TD> Modèle</td><td>  <? echo $template; ?> </TD></TR>
+<TR><TD>Nettoyage des Documents et courriers (ce qui  n'effacera pas les modèles de documents)</TD><TD> <input type="checkbox" name="DOC"></TD></TR>
+<TR><TD>Nettoyage de toutes les fiches (ce qui effacera client, fournisseurs et documents)</TD><TD> <input type="checkbox" name="CARD"></TD></TR>
+<TR><TD>Année </TD><TD><input type="text" size=4 name="YEAR" value=<?echo '"'.$m_date.'"'; ?>></TD></TR>
+    <TR>
+    <TD> <INPUT TYPE=SUBMIT VALUE="Creation Dossier"></TD>
+    </TR>
+    </TABLE>
+ </FORM>
+    
+</div>
