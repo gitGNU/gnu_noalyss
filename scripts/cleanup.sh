eae843d79c82dd68446c7f5e6734cff7ec9a8398
sparkyx <dany118@gmail.com>
2007-04-13 00:31:20 +0000

    add a prefix for dropping

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # clean all phpcompta related DB.
+DOMAIN="test_"
+dropdb -U phpcompta -h localhost ${DOMAIN}account_repository
+dropdb -U phpcompta -h localhost ${DOMAIN}dossier1
+dropdb -U phpcompta -h localhost ${DOMAIN}dossier3
+dropdb -U phpcompta -h localhost ${DOMAIN}dossier4
+dropdb -U phpcompta -h localhost ${DOMAIN}dossier5
+dropdb -U phpcompta -h localhost ${DOMAIN}dossier13
 
-dropdb -U phpcompta -h localhost account_repository
-dropdb -U phpcompta -h localhost dossier1
-dropdb -U phpcompta -h localhost dossier3
-dropdb -U phpcompta -h localhost dossier4
-dropdb -U phpcompta -h localhost dossier5
-dropdb -U phpcompta -h localhost dossier13
-
-dropdb -U phpcompta -h localhost mod1
-dropdb -U phpcompta -h localhost mod2
-dropdb -U phpcompta -h localhost mod3
-dropdb -U phpcompta -h localhost mod7
+dropdb -U phpcompta -h localhost ${DOMAIN}mod1
+dropdb -U phpcompta -h localhost ${DOMAIN}mod2
+dropdb -U phpcompta -h localhost ${DOMAIN}mod3
+dropdb -U phpcompta -h localhost ${DOMAIN}mod7
 
