523667bdb749921614f7eadd91d2331913700019
sparkyx <dany118@gmail.com>
2007-04-15 20:56:36 +0000

    remove starting space

--- contrib/wiki-tools/transform.pl
+++ contrib/wiki-tools/transform.pl
@@ -5,7 +5,7 @@
 #
 #
 use strict;
-
+my $block=0;
 while (my $line=<>) {
 	$line=~s/\<sect\>/!/ ;
 	$line=~s/\<sect1\>/!!/ ;
@@ -25,10 +25,19 @@ while (my $line=<>) {
 	$line=~s/<item>[1-9]\./#/;
 	$line=~s/<item>/\*/;
 	$line=~s/<toc>/(:toc:) (:num:)/;
-	$line=~s/<verb>/[@/;
-	$line=~s/<\/verb>/@]/;
+	if ( $line =~ /<verb>/ ) {
+		$line=~s/<verb>/[@/;	
+		$block=1;
+	}
+	if ( $line =~ /<\/verb>/ ) {
+		$line=~s/<\/verb>/@]/;	
+		$block=0;
+	}
 	$line=~s/<label id=\"(.+)\">/[[#$1]]/;
 	$line=~s/<(ref id=\")(.+)(\") name=\".+\">/[[#$2|Lien]]/;
+	if ( $block eq 0 ) {
+		$line=~s/^ *//;
+	}
 
 	print "$line"; 
 }
