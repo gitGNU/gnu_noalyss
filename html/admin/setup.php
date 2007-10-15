fba2a3c55837404a2dcb589d59089914912614ee
sparkyx <dany118@gmail.com>
2007-10-15 14:40:39 +0000

    Beta 3.0 -3 ready

--- html/admin/setup.php
+++ html/admin/setup.php
@@ -165,7 +165,7 @@ function ExecuteScript($p_cn,$script) {
  */
 function apply_patch($p_cn,$p_name)
 {
-  $MaxVersion=31;
+  $MaxVersion=32;
   for ( $i = 4;$i <= $MaxVersion;$i++)
 	{
 	$to=$i+1;
--- html/index.php
+++ html/index.php
@@ -76,7 +76,7 @@ BODY {
 <script src="js/scripts.js" type="text/javascript"></script>
 </head>
 <BODY onLoad="SetFocus(\'login\',0)">
-Version  3.0-beta
+Version  3.0-beta3
 <div class="remark">
  <p class="gras">Il est conseill&eacute; de ne PAS utiliser Internet Explorer.
  </p><p>
