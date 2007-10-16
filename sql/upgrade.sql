006a6104dfbb0cea9eeb7d8d928c4b5db307ed77
sparkyx <dany118@gmail.com>
2007-10-16 23:38:42 +0000

    Fix column name

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,6 +1,1 @@
-delete from jrn where jr_internal is null;
-delete from jrnx where j_grpt not in (select jr_grpt_id from jrn);
-alter table op_predef add od_direct bool;
-update op_predef set od_direct=false;
-alter table op_predef alter od_direct set not null;
-alter table op_predef_detail add od_qc bool;
+alter table op_predef_detail rename od_qc to opd_qc;
