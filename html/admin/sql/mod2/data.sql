245b6dd375cd5ff424936e9deeb9c32bf0a7b653
sparkyx <dany118@gmail.com>
2007-10-29 18:52:53 +0000

    Fix for postgres 8.1

--- html/admin/sql/mod2/data.sql
+++ html/admin/sql/mod2/data.sql
@@ -1,9 +1,6 @@
 
 SET client_encoding = 'LATIN1';
-SET standard_conforming_strings = off;
 SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET escape_string_warning = off;
 
 SET search_path = public, pg_catalog;
 
