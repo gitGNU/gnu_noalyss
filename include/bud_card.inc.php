71a3c0d5e98cf9c0d22700ce64f4384388323ac1
sparkyx <dany118@gmail.com>
2007-12-18 21:23:51 +0000

    Module Budget : add javascript to widget select

--- include/bud_card.inc.php
+++ include/bud_card.inc.php
@@ -31,6 +31,7 @@ echo '<form method="get">';
 $wHypo=new widget("select","","bh_id");
 $wHypo->value=make_array($cn,"select bh_id,bh_name from bud_hypothese");
 $wHypo->selected=(isset($_REQUEST['bh_id']))?$_REQUEST['bh_id']:"";
+$wHypo->javascript='onChange="this.form.submit();"';
 echo $wHypo->IOValue();
 echo widget::submit_button('recherche','recherche');
 echo dossier::hidden();
