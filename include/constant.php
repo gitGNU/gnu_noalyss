fa126aa3e1d0d65be7f5147bea92358e6690157e
sparkyx <dany118@gmail.com>
2008-02-11 20:11:04 +0000

    remove debug info

--- include/constant.php
+++ include/constant.php
@@ -36,7 +36,7 @@ define ("domaine","");
 define ("MAX_COMPTE",4);
 define ('MAX_BUD_DETAIL',20);
 
-define ("DEBUG","true");
+define ("DEBUG","false");
 
 // securite correspond a la table 
 // action
