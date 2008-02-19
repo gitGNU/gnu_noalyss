a1e36fddd276130266e7115796987d880a6fce5a
Dany De Bontridder <dany@alchimerys.be>
2008-02-19 22:30:47 +0000

    Add a access file (first page) and style the lines of tables are highlighted

--- html/control.php
+++ html/control.php
@@ -59,6 +59,9 @@ switch ($m) {
  case 'pref':
    get_redirect('user_pref.php?'.dossier::get());
    break;
+ case 'access':
+   get_redirect('access.php?'.dossier::get());
+   break;
 
  case 'logout':
    get_redirect('logout.php');
