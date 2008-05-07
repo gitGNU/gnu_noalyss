d8d4919a7c4aee221541bc315d06468b90d29844
Dany De Bontridder <dany@alchimerys.be>
2008-05-07 18:53:32 +0000

    Unicode for doxygen

--- doc/developper/create_doc.sh
+++ doc/developper/create_doc.sh
@@ -1,4 +1,4 @@
 doxygen
-cd html; sed -i "s/iso-8859-1/utf-8/g"
+cd html; sed -i "s/iso-8859-1/utf-8/g" *
 postgresql_autodoc -h localhost -d mod1
 postgresql_autodoc -h localhost -d account_repository
