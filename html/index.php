ac8fe10dbc825ff2578cab6294166872631f2441
Dany De Bontridder <dany@alchimerys.be>
2008-07-25 15:42:01 +0000

    Add version

--- html/index.php
+++ html/index.php
@@ -86,7 +86,7 @@ BODY {
 
 echo '
 <span style="background-color:#879ed4;color:white;padding-left:4px;padding-right:4px;">
-version  4.1-exp
+version  4.1-release-candidate-2127
 </span>
 <BR>
 <BR>
