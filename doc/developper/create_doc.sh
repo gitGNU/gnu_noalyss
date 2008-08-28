90489c9f76697e85e30855a195537c9caf6936df
Dany De Bontridder <dany@alchimerys.be>
2008-08-28 17:55:26 +0000

    correst documentation script

--- doc/developper/create_doc.sh
+++ doc/developper/create_doc.sh
@@ -1,6 +1,6 @@
 doxygen
 cd html; sed -i "s/utf-8/utf-8/g" *
-if [ ! - z "$PGUSER" ] ; then
+if [ ! -z "$PGUSER" ] ; then
 	postgresql_autodoc -u $PGUSER --password $PGPASSWORD -h localhost -d mod1
 	postgresql_autodoc  -u $PGUSER --password $PGPASSWORD  -h localhost -d account_repository
 fi
