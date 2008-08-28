15e5866b64f71a48c29869d86aec11114f9519f5
Dany De Bontridder <dany@alchimerys.be>
2008-08-28 15:52:50 +0000

    add pguser for documentation

--- doc/developper/create_doc.sh
+++ doc/developper/create_doc.sh
@@ -1,4 +1,6 @@
 doxygen
 cd html; sed -i "s/utf-8/utf-8/g" *
-postgresql_autodoc -h localhost -d mod1
-postgresql_autodoc -h localhost -d account_repository
+if [ ! - z "$PGUSER" ] ; then
+	postgresql_autodoc -u $PGUSER --password $PGPASSWORD -h localhost -d mod1
+	postgresql_autodoc  -u $PGUSER --password $PGPASSWORD  -h localhost -d account_repository
+fi
