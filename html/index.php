26e45114aa7873be1698ebd2134fb28b7d8af83c
Dany De Bontridder <dany@alchimerys.be>
2008-08-29 13:57:33 +0000

    update version

--- html/index.php
+++ html/index.php
@@ -86,7 +86,7 @@ BODY {
 
 echo '
 <span style="background-color:#879ed4;color:white;padding-left:4px;padding-right:4px;">
-version  4.1-release-candidate-2131
+version  4.1-release-candidate-2138
 </span>
 <BR>
 <BR>
