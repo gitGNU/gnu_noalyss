fc68b527b1220b61fdc78b88e2b1dd3df1093a73
Dany De Bontridder <dany@alchimerys.be>
2009-04-15 21:41:23 +0000

    Improve add saldo by qcode

--- include/compta_fin.inc.php
+++ include/compta_fin.inc.php
@@ -182,45 +182,77 @@ if ( $def==3) {
   require_once("poste.php");
   require_once ('class_acc_parm_code.php');
   echo '<div class="content">';
-
+  echo '<fieldset><legend>Par poste Comptable</legend>';
   // find the bank account
   // NOTE : those values are in a table because
   // they are _national_ parameters
   $banque=new Acc_Parm_Code($cn,'BANQUE');
   $caisse=new Acc_Parm_Code($cn,'CAISSE');
   $vir_interne=new Acc_Parm_Code($cn,'VIREMENT_INTERNE');
-  $accountSql="select distinct pcm_val::text,pcm_lib from 
+  $accountSql="select distinct pcm_val::text as pcm_val,pcm_lib from 
             tmp_pcmn 
             where pcm_val::text like '".$banque->p_value."%' or pcm_val::text like '".$vir_interne->p_value."%' 
             or pcm_val::text like '".$caisse->p_value."%'
             order by pcm_val::text";
-  $ResAccount=ExecSql($cn,$accountSql);
+  $aFinAccount=get_array($cn,$accountSql);
   echo '<div class="content">';
 
-  echo "<table>";
+  echo "<table class=\"result\">";
   // Filter the saldo
   //  on the current year
-  $filter_year=" and j_tech_per in (select p_id from parm_periode where  p_exercice='".$User->get_exercice()."')";
+  $filter_year="  j_tech_per in (select p_id from parm_periode where  p_exercice='".$User->get_exercice()."')";
 
   // for each account
-  for ( $i = 0; $i < pg_NumRows($ResAccount);$i++) {
+  for ( $i = 0; $i < count($aFinAccount);$i++) {
     // get the saldo
-    $l=pg_fetch_array($ResAccount,$i);
-    $m=get_solde($cn,$l['pcm_val'],$filter_year);
+    $m=get_solde($cn,$aFinAccount[$i]['pcm_val'],' and '.$filter_year);
     // print the result if the saldo is not equal to 0
     if ( $m != 0.0 ) {
       echo "<tr>";
       echo "<TD>".
-	$l['pcm_val'].
+	$aFinAccount[$i]['pcm_val'].
 	"</TD>".
 	"<TD>".
-	$l['pcm_lib'].
+	$aFinAccount[$i]['pcm_lib'].
 	"</TD>"."<TD>".
 	$m.
 	"</TD>"."</TR>";
     }
   }// for
   echo "</table>";
+  echo '</fieldset>';
+  echo '<fieldset><legend>Par fiche (quick_code)</legend>';
+
+  $strAccount='(';
+  foreach ($aFinAccount as $a) {
+    $strAccount.="'".$a['pcm_val']."',";
+  }
+  $strAccount.="'-1')";
+
+  /* find all the quick_code with the corresponding account */
+  $sql='select f_id from fiche join jnt_fic_att_value using (f_id) join attr_value using (jft_id) where ad_id='.ATTR_DEF_ACCOUNT.
+    ' and av_text in '.$strAccount;
+
+  $aFiche=get_array($cn,$sql);
+  echo '<table class="result">';
+  foreach ($aFiche as $k=>$f_id) {
+    
+    $fiche=new fiche($cn,$f_id['f_id']);
+    $saldo_detail=$fiche->get_solde_detail($filter_year);
+    $saldo=( $saldo_detail['debit']>=$saldo_detail['credit'])?$saldo_detail['solde']:$saldo_detail['solde']*(-1);
+    $name=$fiche->getName();
+    $qcode=$fiche->get_quick_code();
+
+?>
+<tr> 
+<td> <?=$qcode?></td>
+<td> <?=$name?> </td>
+<td> <?=$saldo?></td>
+</tr>
+<?php
+  }
+  echo '  </table>';
+  echo '</fieldset>';
   echo "</div>";
   exit();
 }
