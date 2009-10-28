5cba85b50e8adc89d091c2536efbd1b5fe195a15
Dany De Bontridder <dany@alchimerys.be>
2009-10-28 21:55:29 +0000

    Add a new VMS Keytrade from jch

--- include/constant.php
+++ include/constant.php
@@ -26,7 +26,7 @@
 require_once ('config.inc.php');
 require_once('constant.security.php');
 
-define ("DBVERSION",58);
+define ("DBVERSION",59);
 
 define ("MAX_COMPTE",4);
 define ('MAX_BUD_DETAIL',20);
