a51ab994b35ca7469faf80cce182dafd0ab28e08
Dany De Bontridder <dany@alchimerys.be>
2010-04-26 13:11:20 +0000

    back for comptability

--- sql/trunk/reset.sh
+++ sql/trunk/reset.sh
@@ -1,10 +1,10 @@
 dropdb trunk_testdossier13
 dropdb trunk_testmod1
-dropdb trunk_testmod2
-createdb -O trunk trunk_testdossier13
+dropdb runk_testmod2
+createdb -E utf8 -O trunk trunk_testdossier13
 pg_restore -U trunk -O -v -d trunk_testdossier13 ~/Desktop/base\ de\ données/alchimerysdossier2010.bin 
-createdb -O trunk trunk_testmod1
-createdb -O trunk trunk_testmod2
+createdb -E utf8 -O trunk trunk_testmod1
+createdb  -E utf8 -O trunk trunk_testmod2
 pg_restore -U trunk -O -v -d trunk_testmod1 mod1.dmp
 pg_restore -U trunk -O -v -d trunk_testmod2 mod2.dmp
 
