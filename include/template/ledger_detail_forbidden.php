67890eb3a83a4a85e5a8c0d9fd81162193f3eb86
Dany De Bontridder <dany@alchimerys.be>
2010-07-02 23:15:25 +0000

    Security : add forbidden

---
+++ include/template/ledger_detail_forbidden.php
@@ -0,0 +1,8 @@
+<div style="margin:0;padding:0;text-align:right;height:13px">
+<? 
+echo '<A style="background-color:blue;color:white;text-decoration:none" HREF="javascript:void(0)" onclick="removeDiv(\''.$div.'\');">Fermer</A>'; 
+?>
+</div>
+<div style="margin:0;padding:0;background-color:red;text-align:center;">
+<h2 class="error">Accès interdit : vous n'avez pas accès à cette information, contactez votre responsable</h2>;
+</div>
