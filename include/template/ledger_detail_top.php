59ec79f1b678488db1b302f7f9c57607b297f5e6
Dany De Bontridder <dany@alchimerys.be>
2010-07-05 20:09:39 +0000

    Add by default a print bouton

--- include/template/ledger_detail_top.php
+++ include/template/ledger_detail_top.php
@@ -6,8 +6,6 @@
      echo '<A style="margin-right:5px;background-color:blue;color:white;text-decoration:none" HREF="javascript:void(0)" onclick="var a=window.open(\'popup.php?'.$str.'\',\'\',\'toolbar=0,width=600,height=400,scrollbars=yes,resizable=yes,status=0,location=0\'); a.focus();removeDiv(\''.$div.'\')">
 !pop me up ! </A>';
      echo '<A style="background-color:blue;color:white;text-decoration:none" HREF="javascript:void(0)" onclick="removeDiv(\''.$div.'\');">Fermer</A>'; 
-   } else {
-     echo '<a href="javascript:window.print()">Imprimer cette page</a>';
    }
 ?>
 </div>
