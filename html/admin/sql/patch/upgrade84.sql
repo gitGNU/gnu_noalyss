5f33edd1b8ddfa54b437945318a370ff44435847
Dany De Bontridder <dany@alchimerys.be>
2010-07-29 18:20:27 +0000

    Bug : The trigger jrnx_ins does work when the j_qcode is NULL

--- html/admin/sql/patch/upgrade84.sql
+++ html/admin/sql/patch/upgrade84.sql
@@ -44,6 +44,11 @@ $BODY$
 declare
 n_fid bigint;
 begin
+
+if NEW.j_qcode is NULL then
+   return NEW;
+end if;
+
 NEW.j_qcode=trim(upper(NEW.j_qcode));
 
 if length (NEW.j_qcode) = 0 then
@@ -51,7 +56,7 @@ if length (NEW.j_qcode) = 0 then
     else
    select f_id into n_fid from fiche join jnt_fic_att_value using (f_id) join attr_value using(jft_id) where ad_id=23 and av_text=NEW.j_qcode;
 	if NOT FOUND then 
-		raise exception 'La fiche dont le quick code est % n''existe pas',sCode;
+		raise exception 'La fiche dont le quick code est % n''existe pas',NEW.j_qcode;
 	end if;
 end if;
 return NEW;
