6e86b85dcd0a4950a6f78273f13e1f12c13d6e48
Dany De Bontridder <dany@alchimerys.be>
2010-12-07 14:57:27 +0000

    correct in ANC: missing javascript included

--- include/function_javascript.php
+++ include/function_javascript.php
@@ -2752,6 +2752,8 @@ function load_all_script()
     echo js_include('forecast.js');
     echo js_include('gestion.js');
     echo js_include('todo_list.js');
+    echo js_include('anc_script.js');
+
 
 }
 ?>
