e90d97864e112b6e973479e7f35a05f526a58a55
Dany De Bontridder <dany@alchimerys.be>
2010-12-17 23:12:14 +0000

    Call extension in raw, for export a file, print a pdf or export a csv

---
+++ html/extension.raw.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ *   This file is part of PhpCompta.
+ *
+ *   PhpCompta is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   PhpCompta is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with PhpCompta; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/* $Revision$ */
+
+// Copyright Author Dany De Bontridder ddebontridder@yahoo.fr
+
+/*!\file
+ * \brief this file includes the called plugin. It  check first
+ * the security. Load several javascript files
+ */
+require_once('class_database.php');
+require_once('class_dossier.php');
+require_once("ac_common.php");
+require_once("constant.php");
+require_once('function_javascript.php');
+require_once('class_extension.php');
+require_once ('class_html_input.php');
+require_once('class_iselect.php');
+require_once ('constant.security.php');
+require_once ('class_user.php');
+
+$cn=new Database(dossier::id());
+$user=new User($cn);
+$user->check();
+$only_plugin=$user->check_dossier(dossier::id());
+
+$ext=new Extension($cn);
+$ext->search('code',$_REQUEST['plugin_code']);
+if ( $ext->get_parameter('id') != 0 )
+  {
+    /* security */
+    if ( !isset ($_SESSION['g_user']) || $ext->can_request($_SESSION['g_user']) == 0 )
+      {
+	exit();
+      }
+    /* call the ajax script */
+    require_once('ext'.DIRECTORY_SEPARATOR.dirname(trim($ext->get_parameter('filepath'))).DIRECTORY_SEPARATOR.'raw.php');
+  }
+else
+  {
+    alert(j(_("Cette extension n'existe pas ")));
+    exit();
+  }
+?>
