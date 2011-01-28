9649dfeecae25d00d79c91ebc385fa887eb5ef92
Dany De Bontridder <dany@alchimerys.be>
2011-01-28 16:12:20 +0000

    create unique index on quant_* table

--- html/admin/sql/patch/upgrade92.sql
+++ html/admin/sql/patch/upgrade92.sql
@@ -6,5 +6,9 @@ insert into  attr_def(ad_id,ad_text,ad_type,ad_size) values (52,'Contrepartie po
 insert into  attr_def(ad_id,ad_text,ad_type,ad_size) values (53,'Contrepartie pour dépense fiscal. non déd.','poste',22);
 update jrn_def set jrn_def_code=substr(jrn_def_code,1,1)||substr(jrn_def_code,length(jrn_def_code)-1,length(jrn_def_code));
 
+create unique index qs_j_id on quant_sold(j_id);
+create unique index qp_j_id on quant_purchase(j_id);
+create unique index qf_jr_id on quant_fin(jr_id);
+
 update version set val=93;
 commit;
