fa305bcc900cf8fc89feb1193f22f493abd13e8f
Dany De Bontridder <dany@alchimerys.be>
2011-01-28 12:45:26 +0000

    Change jrn_def_code

--- html/admin/sql/patch/upgrade92.sql
+++ html/admin/sql/patch/upgrade92.sql
@@ -4,6 +4,7 @@ insert into  attr_def(ad_id,ad_text,ad_type,ad_size) values (50,'Contrepartie po
 insert into  attr_def(ad_id,ad_text,ad_type,ad_size) values (51,'Contrepartie pour TVA non Ded.','poste',22);
 insert into  attr_def(ad_id,ad_text,ad_type,ad_size) values (52,'Contrepartie pour dépense à charge du gérant','poste',22);
 insert into  attr_def(ad_id,ad_text,ad_type,ad_size) values (53,'Contrepartie pour dépense fiscal. non déd.','poste',22);
+update jrn_def set jrn_def_code=substr(jrn_def_code,1,1)||substr(jrn_def_code,length(jrn_def_code)-1,length(jrn_def_code));
 
 update version set val=93;
 commit;
--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,3 +1,4 @@
 create unique index qs_j_id on quant_sold(j_id);
 create unique index qp_j_id on quant_purchase(j_id);
 create unique index qf_jr_id on quant_fin(jr_id);
+update jrn_def set jrn_def_code=substr(jrn_def_code,1,1)||substr(jrn_def_code,length(jrn_def_code)-1,length(jrn_def_code));
