40c47ed2248a37dd9d648a1aa809599a0650443c
Dany De Bontridder <dany@alchimerys.be>
2011-05-16 16:31:19 +0000

    upgrade93a

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,7 +1,3 @@
-create unique index qs_j_id on quant_sold(j_id);
-create unique index qp_j_id on quant_purchase(j_id);
-create unique index qf_jr_id on quant_fin(jr_id);
-update jrn_def set jrn_def_code=substr(jrn_def_code,1,1)||substr(jrn_def_code,length(jrn_def_code)-1,length(jrn_def_code));
 alter table operation_analytique drop column pa_id;
 ALTER TABLE operation_analytique  ADD CONSTRAINT operation_analytique_oa_amount_check CHECK (oa_amount >= 0::numeric);
 
