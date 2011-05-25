b1d4724b25b52a9f1bd4d9a34289a9de9161f407
Dany De Bontridder <dany@alchimerys.be>
2011-05-25 20:42:41 +0000

    Update version postresql 8.4

--- html/admin/setup.php
+++ html/admin/setup.php
@@ -265,11 +265,11 @@ $version=$cn->get_value($sql);
 var_dump($version);
 
 if ( $version[0] < 8 ||
-     ($version[0]=='8' && $version[2]<2)
+     ($version[0]=='8' && $version[2]<4)
      ) 
   {
 ?>
-  <p> Vous devez absolument utiliser au minimum une version 8.2 de PostGresql, si votre distribution n'en
+  <p> Vous devez absolument utiliser au minimum une version 8.4 de PostGresql, si votre distribution n'en
 offre pas, installez en une en la compilant. </p><p>Lisez attentivement la notice sur postgresql.org pour migrer
 vos bases de donn&eacute;es
 </p>
