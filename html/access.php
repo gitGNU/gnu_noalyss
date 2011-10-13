8de5fe4a6a891836d022c0f9937a7753cfb87919
Dany De Bontridder <dany@alchimerys.be>
2011-10-13 22:13:10 +0000

    format

--- html/access.php
+++ html/access.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  *   This file is part of PhpCompta.
  *
@@ -15,12 +16,12 @@
  *   You should have received a copy of the GNU General Public License
  *   along with PhpCompta; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ */
 /* $Revision: 1615 $ */
 
 // Copyright Author Dany De Bontridder ddebontridder@yahoo.fr
 
-/*!\file
+/* !\file
  * \brief first page
  */
 require_once("class_idate.php");
@@ -37,73 +38,73 @@ require_once("class_itextarea.php");
 require_once('class_calendar.php');
 require_once('class_acc_ledger.php');
 require_once('function_javascript.php');
-$cn=new Database(dossier::id());
-$user=new User($cn);
+$cn = new Database(dossier::id());
+$user = new User($cn);
 $user->Check();
-if ( $user->check_dossier(dossier::id()) == 'P')
+if ($user->check_dossier(dossier::id()) == 'P')
 {
-    redirect("extension.php?".dossier::get(),0);
+    redirect("extension.php?" . dossier::get(), 0);
     exit();
 }
 
 html_page_start($_SESSION['g_theme']);
 load_all_script();
 /*  Check Browser version if < IE6 then unsupported */
-$browser=$_SERVER['HTTP_USER_AGENT'];
-if ( strpos($browser,'MSIE 6')!=false ||
-        strpos($browser,'MSIE 5')!=false )
+$browser = $_SERVER['HTTP_USER_AGENT'];
+if (strpos($browser, 'MSIE 6') != false ||
+	strpos($browser, 'MSIE 5') != false)
 {
 
 
     echo <<<EOF
     <!--[if lt IE 7]>
     <div style='border: 1px solid #F7941D; background: #FEEFDA; text-align: center; clear: both; height: 75px; position: relative;'>
-               <div style='position: absolute; right: 3px; top: 3px; font-family: courier new; font-weight: bold;'><a href='#' onclick='javascript:this.parentNode.parentNode.style.display="none"; return false;'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-cornerx.jpg' style='border: none;' alt='Close this notice'/></a></div>
-                          <div style='width: 640px; margin: 0 auto; text-align: left; padding: 0; overflow: hidden; color: black;'>
-                                     <div style='width: 75px; float: left;'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-warning.jpg' alt='Warning!'/></div>
-                                                <div style='width: 275px; float: left; font-family: Arial, sans-serif;'>
-                                                           <div style='font-size: 14px; font-weight: bold; margin-top: 12px;'>Vous utilisez un navigateur dépassé depuis près de 8 ans!</div>
-                                                                      <div style='font-size: 12px; margin-top: 6px; line-height: 12px;'>Pour une meilleure expérience web, prenez le temps de mettre votre navigateur à jour.</div>
-                                                                                 </div>
-                                                                                 <div style='width: 75px; float: left;'><a href='http://fr.www.mozilla.com/fr/' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-firefox.jpg' style='border: none;' alt='Get Firefox 3.5'/></a></div>
-                                                                                            <div style='width: 73px; float: left;'><a href='http://www.apple.com/fr/safari/download/' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-safari.jpg' style='border: none;' alt='Get Safari 4'/></a></div>
-                                                                                                       <div style='float: left;'><a href='http://www.google.com/chrome?hl=fr' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-chrome.jpg' style='border: none;' alt='Get Google Chrome'/></a></div>
-                                                                                                                                         </div>
-                                                                                                                                         </div>
-                                                                                                                                         <![endif]-->
+    <div style='position: absolute; right: 3px; top: 3px; font-family: courier new; font-weight: bold;'><a href='#' onclick='javascript:this.parentNode.parentNode.style.display="none"; return false;'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-cornerx.jpg' style='border: none;' alt='Close this notice'/></a></div>
+    <div style='width: 640px; margin: 0 auto; text-align: left; padding: 0; overflow: hidden; color: black;'>
+    <div style='width: 75px; float: left;'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-warning.jpg' alt='Warning!'/></div>
+    <div style='width: 275px; float: left; font-family: Arial, sans-serif;'>
+   <div style='font-size: 14px; font-weight: bold; margin-top: 12px;'>Vous utilisez un navigateur dépassé depuis près de 8 ans!</div>
+    <div style='font-size: 12px; margin-top: 6px; line-height: 12px;'>Pour une meilleure expérience web, prenez le temps de mettre votre navigateur à jour.</div>
+    </div>
+   <div style='width: 75px; float: left;'><a href='http://fr.www.mozilla.com/fr/' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-firefox.jpg' style='border: none;' alt='Get Firefox 3.5'/></a></div>
+   <div style='width: 73px; float: left;'><a href='http://www.apple.com/fr/safari/download/' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-safari.jpg' style='border: none;' alt='Get Safari 4'/></a></div>
+ <div style='float: left;'><a href='http://www.google.com/chrome?hl=fr' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-chrome.jpg' style='border: none;' alt='Get Google Chrome'/></a></div>
+     </div>
+     </div>
+     <![endif]-->
 EOF;
-exit();
-}
-if ( $cn->exist_table('version') == false )
-{
-    echo '<h2 class="error" style="font-size:12px">'._("Base de donnée invalide").'</h2>';
-    $base=dirname($_SERVER['REQUEST_URI']);
-    echo HtmlInput::button_anchor('Retour',$base.'/user_login.php');
     exit();
 }
-if ( DBVERSION < dossier::get_version($cn))
+if ($cn->exist_table('version') == false)
 {
-    echo '<h2 class="error" style="font-size:12px">'._("Attention: la version de base de donnée est supérieure à la version du programme, vous devriez mettre à jour").'</h2>';
+    echo '<h2 class="error" style="font-size:12px">' . _("Base de donnée invalide") . '</h2>';
+    $base = dirname($_SERVER['REQUEST_URI']);
+    echo HtmlInput::button_anchor('Retour', $base . '/user_login.php');
+    exit();
 }
-if ( DBVERSION > dossier::get_version($cn))
+if (DBVERSION < dossier::get_version($cn))
 {
-    echo '<h2 class="error" style="font-size:12px">'._("Votre base de données n'est pas à jour").'   ';
-    $a=_("cliquez ici pour appliquer le patch");
-    $base=dirname($_SERVER['REQUEST_URI']).'/admin/setup.php';
-    echo '<a hreF="'.$base.'">'.$a.'</a></h2>';
+    echo '<h2 class="error" style="font-size:12px">' . _("Attention: la version de base de donnée est supérieure à la version du programme, vous devriez mettre à jour") . '</h2>';
+}
+if (DBVERSION > dossier::get_version($cn))
+{
+    echo '<h2 class="error" style="font-size:12px">' . _("Votre base de données n'est pas à jour") . '   ';
+    $a = _("cliquez ici pour appliquer le patch");
+    $base = dirname($_SERVER['REQUEST_URI']) . '/admin/setup.php';
+    echo '<a hreF="' . $base . '">' . $a . '</a></h2>';
 }
 
 /*
  * Set a correct periode for the user
  */
-$periode=$user->get_periode();
-$oPeriode=new Periode($cn,$periode);
+$periode = $user->get_periode();
+$oPeriode = new Periode($cn, $periode);
 
-if ($oPeriode->load() == -1 )
-  {
-    $periode=$cn->get_value('select p_id from parm_periode order by p_start asc limit 1');
+if ($oPeriode->load() == -1)
+{
+    $periode = $cn->get_value('select p_id from parm_periode order by p_start asc limit 1');
     $user->set_periode($periode);
-  }
+}
 
 echo '<div class="u_tmenu">';
 echo menu_tool('access.php');
@@ -111,23 +112,23 @@ echo '</div>';
 echo '<div class="content">';
 
 /* others report */
-$cal=new Calendar();
+$cal = new Calendar();
 $cal->get_preference();
-$Ledger=new Acc_Ledger($cn,0);
-$last_ledger=array();
-if ( $user->check_action(GESTION)==1)
-  {
-    $Operation=new Action($cn);
-    $last_ledger=$Ledger->get_last(10);
-    $last_operation=$Operation->get_last(10);
-  }
+$Ledger = new Acc_Ledger($cn, 0);
+$last_ledger = array();
+if ($user->check_action(GESTION) == 1)
+{
+    $Operation = new Action($cn);
+    $last_ledger = $Ledger->get_last(10);
+    $last_operation = $Operation->get_last(10);
+}
 else
-  {
-    $last_operation=array();
-  }
+{
+    $last_operation = array();
+}
 ob_start();
 require_once('template/dashboard.php');
-$ret=ob_get_contents();
+$ret = ob_get_contents();
 ob_end_clean();
 echo $ret;
 
