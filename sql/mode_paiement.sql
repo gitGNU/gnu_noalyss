b23783bf838ff3ff385df864c802bc7a0903a165
Dany De Bontridder <dany@alchimerys.be>
2011-11-15 13:28:49 +0000

    SQL Script: #447 mode de paiement par journaux -

---
+++ sql/mode_paiement.sql
@@ -0,0 +1,13 @@
+-- Mode de paiement par journaux
+
+
+alter table mod_payment add jrn_def_id bigint;
+update mod_payment set jrn_def_id=2 where mp_type='VEN';
+update mod_payment set jrn_def_id=3 where mp_type='ACH';
+
+alter table mod_payment drop mp_type;
+
+
+alter table mod_payment add constraint foreign key (jrn_def_id) references jrn_def(jrn_def_id);
+
+comment on column mod_payment.jrn_def_id is 'Ledger using this payment method';
