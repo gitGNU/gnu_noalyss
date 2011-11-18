0d758bea7d34a3e0f29dba93276f969d5e197549
Dany De Bontridder <dany@alchimerys.be>
2011-11-18 12:57:33 +0000

    #462 : autocompletion pour les activités

---
+++ include/ajax_auto_anc_card.php
@@ -0,0 +1,20 @@
+<?
+require_once ("class_database.php");
+require_once 'class_user.php';
+$cn=new Database($_GET['gDossier']);
+$user=new User($cn);
+$user->Check();
+$user->check_dossier($_GET['gDossier']);
+$res=$cn->exec_sql("select po_id,po_description from  poste_analytique where pa_id=$1 ~* and (po_description ~*$2 or po_id ~* $3 limit 8",
+        array($_REQUEST['pa_id'],$_POST['anccard'],$_POST['anccard']));
+$nb=Database::num_row($res);
+	echo "<ul>";
+for ($i = 0;$i< $nb;$i++)
+{
+	$row=Database::fetch_array($res,$i);
+	echo "<li>";
+	echo $row['po_id'];
+	echo '<span class="informal"> '.$row['po_description'].'</span></li>';
+}
+	echo "</ul>";
+?>
