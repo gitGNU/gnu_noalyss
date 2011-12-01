143a83905de9369a9a2b7d236c4c93491fb4f3c0
Dany De Bontridder <dany@alchimerys.be>
2011-12-01 00:33:30 +0000

    change L user to R user

--- html/admin/sql/patch/ac-upgrade13.sql
+++ html/admin/sql/patch/ac-upgrade13.sql
@@ -5,6 +5,6 @@ begin;
 ALTER TABLE audit_connect ADD CONSTRAINT valid_state CHECK (ac_state = 'FAIL'::text OR ac_state = 'SUCCESS'::text or ac_state='AUDIT');
 -- run to the account_repository
 insert into theme values ('EPad','style-epad.css',null);
-
+update priv_user set priv_priv='R' where priv_priv='L';
 update version set val=14;
 commit;
