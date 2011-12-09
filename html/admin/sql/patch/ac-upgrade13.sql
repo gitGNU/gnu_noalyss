47633fdbced7eba03dd0199b90b17113db4588df
Dany De Bontridder <dany@alchimerys.be>
2011-12-09 22:56:32 +0000

    Update user from P to R

--- html/admin/sql/patch/ac-upgrade13.sql
+++ html/admin/sql/patch/ac-upgrade13.sql
@@ -6,5 +6,6 @@ ALTER TABLE audit_connect ADD CONSTRAINT valid_state CHECK (ac_state = 'FAIL'::t
 -- run to the account_repository
 insert into theme values ('EPad','style-epad.css',null);
 update priv_user set priv_priv='R' where priv_priv='L';
+update priv_user set priv_priv='R' where priv_priv='P';
 update version set val=14;
 commit;
