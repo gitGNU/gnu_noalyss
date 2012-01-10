5560d1553b6d033b995f93eb4b8a75f329d2ab15
Dany De Bontridder <dany@alchimerys.be>
2012-01-10 18:06:06 +0000

    update script for report definition CSV:reportinit

--- html/admin/sql/patch/upgrade100.sql
+++ html/admin/sql/patch/upgrade100.sql
@@ -17,6 +17,11 @@ ALTER TABLE jnt_fic_attr
       REFERENCES attr_def (ad_id) MATCH SIMPLE
       ON UPDATE cascade ON DELETE cascade;
 
+insert into menu_ref(me_code,me_menu,me_type) values ('CVS:reportinit','Export définition d''un raport','PR');
+
+insert  into profile_menu (me_code,p_id,p_type_display) values ('CVS:reportinit',1,'P');
+insert  into profile_menu (me_code,p_id,p_type_display) values ('CVS:reportinit',2,'P');
+
 update version set val=101;
 
 commit;
