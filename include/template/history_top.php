2928c8532c9ef4eef68ce1825b34ecf6e2b0ffa9
Dany De Bontridder <dany@alchimerys.be>
2012-06-16 17:36:55 +0000

    change pop me up by pop me out

--- include/template/history_top.php
+++ include/template/history_top.php
@@ -5,7 +5,7 @@
      $callback=$_SERVER['PHP_SELF'];
      $str=$_SERVER['QUERY_STRING']."&act=de&ajax=$callback";
      echo '<A id="close_div" HREF="javascript:void(0)" onclick="var a=window.open(\'popup.php?'.$str.'\',\'\',\'toolbar=0,width=600,height=400,scrollbars=yes,resizable=yes,status=0,location=0\'); a.focus();removeDiv(\''.$div.'\')">
-!pop me up ! </A>';
+!pop me out ! </A>';
 echo '<A id="close_div" HREF="javascript:void(0)" onclick="removeDiv(\''.$div.'\');">Fermer</A>';
 }
 ?>
