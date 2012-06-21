d341216e57a24358b6a72df3827057ba74cf4e6d
Dany De Bontridder <dany@alchimerys.be>
2012-06-21 14:39:19 +0000

    Add missing tab

--- include/ajax_create_menu.php
+++ include/ajax_create_menu.php
@@ -3,6 +3,7 @@ require_once 'class_menu_ref.php';
 $msg="Création";
 $m=new Menu_Ref($cn);
 echo '<form method="POST" onsubmit="return confirm(\'Vous confirmez ?\')">';
+echo HtmlInput::hidden('tab','profile_menu_div');
 require_once 'template/menu_detail.php';
 echo HtmlInput::submit('create_menu','Sauver');
 echo HtmlInput::button_close('divmenu');
