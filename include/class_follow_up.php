8c3cfd79f7c611092ccccd0e74912fc97a93a240
Dany De Bontridder <dany@alchimerys.be>
2012-06-30 17:03:29 +0000

    bug follow_up::get_late

--- include/class_follow_up.php
+++ include/class_follow_up.php
@@ -1019,7 +1019,7 @@ class Follow_Up
          */
 	function get_today()
 	{
-		$sql = "select coalesce(vw_name,'Interne') as vw_name,ag_id,ag_title,ag_ref, dt_value,to_char(ag_timestamp,'DD.MM.YYYY') as ag_timestamp_fmt,ag_timestamp " .
+		$sql = "select coalesce(vw_name,'Interne') as vw_name,ag_id,ag_title,ag_ref, dt_value,to_char(ag_remind_date,'DD.MM.YYYY') as ag_timestamp_fmt,ag_timestamp " .
 				" from action_gestion join document_type " .
 				" on (ag_type=dt_id) left join vw_fiche_attr on (f_id=f_id_dest) where ag_state not in (1,4)
 					and to_char(ag_remind_date,'DDMMYYYY')=to_char(now(),'DDMMYYYY') ";
@@ -1032,7 +1032,7 @@ class Follow_Up
          */
 	function get_late()
 	{
-		$sql = "select coalesce(vw_name,'Interne') as vw_name,ag_id,ag_title,ag_ref, dt_value,to_char(ag_timestamp,'DD.MM.YYYY') as ag_timestamp_fmt,ag_timestamp " .
+		$sql = "select coalesce(vw_name,'Interne') as vw_name,ag_id,ag_title,ag_ref, dt_value,to_char(ag_remind_date,'DD.MM.YYYY') as ag_timestamp_fmt,ag_timestamp " .
 				" from action_gestion join document_type " .
 				" on (ag_type=dt_id) left join vw_fiche_attr on (f_id=f_id_dest) where ag_state not in  (1,4)
 				and ag_remind_date > now() ";
