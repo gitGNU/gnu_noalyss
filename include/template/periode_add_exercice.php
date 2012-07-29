84b70973f14cd8dc0a3a81c8fb54a057504521bf
Dany De Bontridder <dany@alchimerys.be>
2012-07-29 13:08:11 +0000

    Message for exercice

--- include/template/periode_add_exercice.php
+++ include/template/periode_add_exercice.php
@@ -2,8 +2,11 @@
 <ul>
 <li>
 Exercice en 12 périodes : Ajout d'un exercice comptable de 12 périodes, commençant le 1 janvier et terminant le 31 décembre. </li>
-<li>   Exercice en 13 périodes: Ajout d'une période d'un jour le 31/12. Cette période est utilisée 
+<li>   Exercice en 13 périodes: Ajout d'une période d'un jour le 31/12. Cette période est utilisée
 pour faire toutes les écritures de fin d'exercice: amortissements, régulations de compte... Avec une 13ième période, cela simplifie les prévisions, les rapports...</li>
+<li>
+	Pour ajouter des années, ne commençant pas en janvier ou comptant un nombre de mois supérieur à 12, utilisez le plugin "Outils Comptables"
+</li>
 </ul>
 
 <form method="post" onsubmit="return confirm('Confirmez vous l\'ajout d\'un exercice comptable ?')">
