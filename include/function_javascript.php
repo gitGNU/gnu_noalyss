337f0bafe63de5e8582b2079f2a2b119ab9914a7
Dany De Bontridder <dany@alchimerys.be>
2012-10-30 19:43:43 +0000

    add niceEdit.js

--- include/function_javascript.php
+++ include/function_javascript.php
@@ -2756,6 +2756,7 @@ function load_all_script()
     echo js_include('todo_list.js');
     echo js_include('anc_script.js');
     echo js_include('sorttable.js');
+    echo js_include('nicEdit.js');
 
 
 }
