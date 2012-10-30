f9d06706cb3d388122fd04fcb28438a78bba4b22
Dany De Bontridder <dany@alchimerys.be>
2012-10-30 21:20:07 +0000

    0000744: (classe) 5Actif 0000746: ajouter un espace là où il faut

--- include/user_menu.php
+++ include/user_menu.php
@@ -135,15 +135,15 @@ function menu_acc_plan($p_start=1)
     $base="?ac=".$_REQUEST['ac'];
     $str_dossier="&".dossier::get();
     echo '<TABLE>';
-    echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=0'.$str_dossier.'">0'._(' Hors Bilan').' </A></TD></TR>';
-    echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=1'.$str_dossier.'">1'._(' Immobilisé').' </A></TD></TR>';
+    echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=0'.$str_dossier.'">0 '._(' Hors Bilan').' </A></TD></TR>';
+    echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=1'.$str_dossier.'">1 '._(' Immobilisé').' </A></TD></TR>';
     echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=2'.$str_dossier.'">2 '._('Actif a un an au plus').'</A></TD></TR>';
     echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=3'.$str_dossier.'">3 '._('Stock et commande').'</A></TD></TR>';
     echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=4'.$str_dossier.'">4 '._('Compte tiers').'</A></TD></TR>';
-    echo '<TR><TD class="mtitle"><A class="mtitle" HREF="'.$base.'&p_start=5'.$str_dossier.'">5'._('Actif').'</A></TD></TR>';
-    echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=6'.$str_dossier.'">6'._('Charges').'</A></TD></TR>';
-    echo '<TR><TD class="mtitle"><A class="mtitle" HREF="'.$base.'&p_start=7'.$str_dossier.'">7'._('Produits').'</A></TD></TR>';
-    echo '<TR><TD class="mtitle"><A class="mtitle" HREF="'.$base.'&p_start=8'.$str_dossier.'">8'._('Hors Comptabilité').'</A></TD></TR>';
+    echo '<TR><TD class="mtitle"><A class="mtitle" HREF="'.$base.'&p_start=5'.$str_dossier.'">5 '._('Financier').'</A></TD></TR>';
+    echo '<TR><TD class="mtitle"><A class="mtitle"  HREF="'.$base.'&p_start=6'.$str_dossier.'">6 '._('Charges').'</A></TD></TR>';
+    echo '<TR><TD class="mtitle"><A class="mtitle" HREF="'.$base.'&p_start=7'.$str_dossier.'">7 '._('Produits').'</A></TD></TR>';
+    echo '<TR><TD class="mtitle"><A class="mtitle" HREF="'.$base.'&p_start=8'.$str_dossier.'">8 '._('Hors Comptabilité').'</A></TD></TR>';
     echo '<TR><TD class="mtitle"><A class="mtitle" HREF="'.$base.'&p_start=9'.$str_dossier.'">9 '._('Hors Comptabilité').'</A></TD></TR>';
     echo "</TABLE>";
 }
