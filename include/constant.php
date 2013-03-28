6fa795bbb7f7f27e86e43cf975df61d125164669
Dany De Bontridder <dany@alchimerys.be>
2013-03-28 17:05:08 +0000

    Add patch upgrade105

--- include/constant.php
+++ include/constant.php
@@ -35,19 +35,19 @@ $g_failed="<span style=\"font-size:18px;color:red\">&#x2716;</span>";
 $g_succeed="<span style=\"font-size:18px;color:green\">&#x2713;</span>";
 /*set to none for production */
 /* uncomment for production */
-// $version_phpcompta=SVNINFO;
-// define ("DEBUG",false);
- define ('SVNINFO',5138);
- $version_phpcompta=SVNINFO;
- define ("DEBUG",true);
-
+$version_phpcompta=SVNINFO;
+define ("DEBUG",false);
+/* define ('SVNINFO',5015);
+ * $version_phpcompta=SVNINFO;
+ * define ("DEBUG",true);
+*/
 // If you don't want to be notified of the update
 // define ("SITE_UPDATE",'');
 define ("SITE_UPDATE",'http://www.phpcompta.eu/last_version.txt');
 define ("SITE_UPDATE_PLUGIN",'http://www.phpcompta.eu/plugin_last_version.txt');
 
 
-define ("DBVERSION",105);
+define ("DBVERSION",106);
 
 define ("DBVERSIONREPO",14);
 define ('NOTFOUND','--not found--');
