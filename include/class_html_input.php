245ac30e6efb4b0db9e63842831b39ea8d0f2574
Dany De Bontridder <dany@alchimerys.be>
2013-05-11 22:52:49 +0000

    Filtre dynamique: ajout bouton reset

--- include/class_html_input.php
+++ include/class_html_input.php
@@ -717,7 +717,8 @@ class HtmlInput
 	{
 		$r= "
 			<form style=\"display:inline\">
-			<input class=\"input_text\" name=\"filter\" onkeyup=\"filter_table(this, '$p_table_id','$p_col',$start_row )\" type=\"text\">
+			<input id=\"lk_".$p_table_id."\" class=\"input_text\" name=\"filter\" onkeyup=\"filter_table(this, '$p_table_id','$p_col',$start_row )\" type=\"text\">
+			<input type=\"button\" onclick=\"$('lk_".$p_table_id."').value='';filter_table($('lk_".$p_table_id."'), '$p_table_id','$p_col',$start_row );\" value=\"X\">
 			</form>
 			";
 		return $r;
