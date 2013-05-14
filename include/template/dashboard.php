10970276fd1fe3ca004aa506ed9bef14d39f95e7
Dany De Bontridder <dany@alchimerys.be>
2013-05-14 20:53:30 +0000

    change height mini-report

--- include/template/dashboard.php
+++ include/template/dashboard.php
@@ -130,7 +130,7 @@ if ( $rapport->exist() == false ) {
 }
 
 if ( $report != 0 ) {
-  echo '<fieldset style="height:70%;"><legend>'.$rapport->get_name().'</legend>';
+  echo '<fieldset style="height:50%;"><legend>'.$rapport->get_name().'</legend>';
   $exercice=$g_user->get_exercice();
   if ( $exercice == 0 ) {
     alert(_('Aucune periode par defaut'));
@@ -155,7 +155,7 @@ if ( $report != 0 ) {
   echo '</fieldset>';
   echo '</div>';
  } else {
-  echo '<fieldset style="height:70%;width:80%;background-color:white"><legend>'._('Aucun rapport défini').'</legend>';
+  echo '<fieldset style="height:60%;width:80%;background-color:white"><legend>'._('Aucun rapport défini').'</legend>';
   echo '<a href="do.php?'.dossier::get().'&ac=PREFERENCE">'._('Cliquez ici pour mettre à jour vos préférences').'</a>';
 
 
