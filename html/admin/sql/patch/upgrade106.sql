f4782b7bf7b144f4c9eea4e8fecd36325bda2ff2
Dany De Bontridder <dany@alchimerys.be>
2013-05-18 20:20:48 +0000

    task #0000844: Ajout d'une description pour les catégories de fiches

--- html/admin/sql/patch/upgrade106.sql
+++ html/admin/sql/patch/upgrade106.sql
@@ -6,11 +6,11 @@ ALTER TABLE mod_payment  DROP CONSTRAINT mod_payment_mp_jrn_def_id_fkey ;
 ALTER TABLE mod_payment  ADD CONSTRAINT mod_payment_mp_jrn_def_id_fkey FOREIGN KEY (mp_jrn_def_id)      REFERENCES jrn_def (jrn_def_id) MATCH SIMPLE ON UPDATE 	CASCADE ON DELETE CASCADE;
 ALTER TABLE fiche_def ADD COLUMN fd_description text;
 update fiche_def set fd_description='Achats de marchandises' where fd_id=1;
-update fiche_def set fd_description='Liste des clients' where fd_id=2;
-update fiche_def set fd_description='Liste des comptes financiers: banque, caisse,...' where fd_id=3;
-update fiche_def set fd_description='Liste des fournisseurs' where fd_id=4;
-update fiche_def set fd_label='Services & Biens Divers',fd_description='Pour les charges diverses' where fd_id=5;
-update fiche_def set fd_description='Vente de prestations, marchandises ' where fd_id=6;
+update fiche_def set fd_description='Catégorie qui contient la liste des clients' where fd_id=2;
+update fiche_def set fd_description='Catégorie qui contient la liste des comptes financiers: banque, caisse,...' where fd_id=3;
+update fiche_def set fd_description='Catégorie qui contient la liste des fournisseurs' where fd_id=4;
+update fiche_def set fd_label='Services & Biens Divers',fd_description='Catégorie qui contient la liste des charges diverses' where fd_id=5;
+update fiche_def set fd_description='Catégorie qui contient la liste des prestations, marchandises... que l''on vend ' where fd_id=6;
 update version set val=107;
 
 commit;
