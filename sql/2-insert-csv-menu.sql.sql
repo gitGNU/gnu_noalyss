ff22aa42cbf74e09d4c0f3f2fd317c317d99a0dd
Dany De Bontridder <dany@alchimerys.be>
2013-10-23 14:39:56 +0000

    make patch upgrade108.sql with them

--- sql/2-insert-csv-menu.sql.sql
+++
@@ -1,7 +0,0 @@
-insert into menu_ref(me_code,me_menu,me_file, me_url,me_description,me_parameter,me_javascript,me_type,me_description_etendue)
-values
-('CSV:Reconciliation','Export opérations rapprochées','export_rec_csv.php',null,'Export opérations rapprochées en CSV',null,null,'PR','');
-
-insert into profile_menu (me_code,me_code_dep,p_id,p_order, p_type_display,pm_default) 
-values
-('CSV:Reconciliation',null,1,0,'P',0), ('CSV:Reconciliation',null,2,null,'P',0);
--- sql/3-stock-col-change.sql
+++
@@ -1,1 +0,0 @@
-alter table stock_change alter tech_date type time;
