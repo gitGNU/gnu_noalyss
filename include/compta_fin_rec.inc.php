90798a29bb6b8f28cdb58c7798b535e7d764211f
Dany De Bontridder <dany@alchimerys.be>
2013-12-06 11:07:16 +0000

    FREC : tri incorrect

--- include/compta_fin_rec.inc.php
+++ include/compta_fin_rec.inc.php
@@ -151,7 +151,7 @@ for ($i = 0; $i < count($operation); $i++)
 	$r.=td($row['jr_comment']);
 	$r.=td($js);
 	$amount=$cn->get_value('select qf_amount from quant_fin where jr_id=$1', array($row['jr_id']));
-	$r.='<td class="num" class="sorttable_numeric" sortable_customekey="'.$amount.'" style="text-align:right">'.nbm ($amount).'</td>';
+	$r.='<td class="num" class="sorttable_numeric" sorttable_customkey="'.$amount.'" style="text-align:right">'.nbm ($amount).'</td>';
 
 	$diff=bcadd($diff,$amount);
 	$tot_not_reconcilied+=$row['jr_montant'];
