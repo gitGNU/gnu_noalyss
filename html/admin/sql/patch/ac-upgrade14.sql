261d9ffde73ae0eea1e7c807d2811f77ad66d3a2
Dany De Bontridder <dany@alchimerys.be>
2014-03-06 22:09:01 +0100

    Improve appareance and set Light as the default theme

--- html/admin/sql/patch/ac-upgrade14.sql
+++ html/admin/sql/patch/ac-upgrade14.sql
@@ -1,8 +1,7 @@
 begin;
 update theme set the_name='Mandarine' ,the_filestyle='style-mandarine.css' where the_name='Colored';
 update theme set the_name='Mobile' ,the_filestyle='style-mobile.css' where the_name='EPad';
-update user_global_pref set parameter_value='Mandarine' where parameter_value='Colored';
-update user_global_pref set parameter_value='Mobile' where parameter_value='EPad';
+update user_global_pref set parameter_value='Light' where parameter_type='THEME';
 update theme set the_filestyle='style-classic.css' where the_filestyle='style.css';
 update version set val=15;
 commit;
