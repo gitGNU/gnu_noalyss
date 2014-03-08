0daabf6760fe2fe7680a2bcaac20aca6ade7fbd5
Dany De Bontridder <dany@alchimerys.be>
2014-03-08 16:09:08 +0100

    default theme = Classique

--- html/admin/sql/patch/ac-upgrade14.sql
+++ html/admin/sql/patch/ac-upgrade14.sql
@@ -1,7 +1,8 @@
 begin;
 update theme set the_name='Mandarine' ,the_filestyle='style-mandarine.css' where the_name='Colored';
 update theme set the_name='Mobile' ,the_filestyle='style-mobile.css' where the_name='EPad';
-update user_global_pref set parameter_value='Light' where parameter_type='THEME';
+update theme set the_name = 'Classique' where the_name='classic';
+update user_global_pref set parameter_value='Classique' where parameter_type='THEME';
 update theme set the_filestyle='style-classic.css' where the_filestyle='style.css';
 update version set val=15;
 commit;
