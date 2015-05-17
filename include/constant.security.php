90c51a511f0f95166c49cc0f1982a82dda2cc25a
Dany De Bontridder <dany@alchimerys.be>
2015-05-17 17:05:38 +0200

    Task #1119 - Possibilité d'effacer des documents attachés à des opérations Empêche effacement document attaché à opération comptable. Réutilisation du privilège dans suivi

--- include/constant.security.php
+++ include/constant.security.php
@@ -21,7 +21,7 @@
 define ('FICADD',800);	 /* Ajout de fiche */
 define ("FIC",805);  //Création, modification et effacement de fiche
 define ("FICCAT",910);  //création, modification et effacement de catégorie de fiche
-define ('RMDOC',1020);   // Effacement de document pour follow up
+define ('RMDOC',1020);   // Effacement de document pour follow up & comptabilité
 define ('VIEWDOC',1010);   // Voir document pour follow up
 define ('PARCATDOC',1050);   // modifier type document pour follow up
 global $audit; $audit=false;
