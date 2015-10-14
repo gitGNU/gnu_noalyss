3711b7770b2ecbac265f5fe8d150b66b8b38534b
Dany De Bontridder <dany@alchimerys.be>
2015-10-14 18:43:47 +0200

    Security : check dossier access

--- html/show_document_modele.php
+++ html/show_document_modele.php
@@ -34,6 +34,8 @@ require_once NOALYSS_INCLUDE.'/class/class_user.php';
 global $g_user;
 $g_user=new User($cn);
 $g_user->Check();
+$g_user->check_dossier($gDossier);
+
 set_language();
 if ( $g_user->check_module("CFGDOC") == 0 ) exit();
 // retrieve the document
